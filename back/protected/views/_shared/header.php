f1542d1de8354a3d2494448a7299408751741ea2
Tung Nguyen <thanhtung228@gmail.com>
2013-01-28 16:29:46 +0700

    Remove loading

--- back/protected/views/_shared/header.php
+++ back/protected/views/_shared/header.php
@@ -51,16 +51,7 @@
     </head>
 
     <body>
-        <div id="overlay">
-            <ul>
-                <li class="li1"></li>
-                <li class="li2"></li>
-                <li class="li3"></li>
-                <li class="li4"></li>
-                <li class="li5"></li>
-                <li class="li6"></li>
-            </ul>
-        </div>	
+
         <!-- start: Header -->
         <div class="navbar">
             <div class="navbar-inner">
